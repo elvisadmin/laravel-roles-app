5c10bb8b69fa18f02b3c3a089f478be54cf4874a
etechsolutions <elvis.pionita@gmail.com>
2026-01-21 22:38:15 +0800

    neew merge 2

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -3,10 +3,22 @@
 namespace ElvisAdmin\LaravelRoles\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Role extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($role) {
+            if (!$role->slug) {
+                $role->slug = Str::slug($role->name);
+            }
+        });
+    }
 
     public function users()
     {
