33a47bf7b04f60a8fafb452cc803d283e4da025b
etechsolutions <elvis.pionita@gmail.com>
2026-01-21 21:50:39 +0800

    src

---
+++ src/LaravelRolesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ElvisAdmin\LaravelRoles;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRolesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // bind things if needed
+    }
+
+    public function boot()
+    {
+        // publish migrations, etc.
+    }
+}
